08d962c8549758bc213935166e69568f9218a1c9
colton <51266557+coltonrusch@users.noreply.github.com>
2020-04-21 00:54:54 -0400

    Added basic chat functionality to build upon

---
+++ src/main/java/edu/brown/cs/crusch/chat/Main.java
@@ -0,0 +1,8 @@
+package edu.brown.cs.crusch.chat;
+
+public class Main {
+  public static void main(String[] args) {
+    Chat chat = new Chat();
+    Chat.main(new String[]{});
+  }
+}
---
+++ src/main/resources/public/websocketDemo.js
@@ -0,0 +1,42 @@
+// establish WebSocket connection & event listeners
+const webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
+webSocket.onmessage = function (msg) { updateChat(msg); };
+webSocket.onclose = function () { alert("WebSocket connection closed") };
+
+// click "send" --> send message
+id("send").addEventListener("click", function () {
+    sendMessage(id("message").value);
+});
+
+// enter is pressed --> send message
+id("message").addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) { sendMessage(e.target.value); }
+});
+
+// sends the message if not empty, clears input field
+function sendMessage(message) {
+    if (message !== "") {
+        webSocket.send(message);
+        id("message").value = "";
+    }
+}
+
+// updates chat window and list of connected users
+function updateChat(msg) {
+    var data = JSON.parse(msg.data);
+    insert("chat", data.userMessage);
+    id("userlist").innerHTML = "";
+    data.userlist.forEach(function (user) {
+        insert("userlist", "<li>" + user + "</li>");
+    });
+}
+
+// helper --> inserts HTML as first child of an element
+function insert(targetId, message) {
+    id(targetId).insertAdjacentHTML("afterbegin", message);
+}
+
+// helper --> gets an element by an id
+function id(id) {
+    return document.getElementById(id);
+}
