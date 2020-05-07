e9a738c856745a1a1e336aec6495516ee75841a6
suhyepark98 <suhyepark98@gmail.com>
2020-05-07 10:04:36 +0900

    Added graph components

--- src/main/java/edu/brown/cs/student/api/graph/GraphNode.java
+++ src/main/java/edu/brown/cs/student/api/graph/GraphNode.java
@@ -1,155 +1,148 @@
-//package edu.brown.cs.student.api.graph;
-//
-//import java.util.List;
-//import java.util.concurrent.ExecutionException;
-//
-//import com.google.common.cache.LoadingCache;
-//
-//import edu.brown.cs.student.graph.IVertex;
-//
-///**
-// * A class representing a GraphNode object.
-// */
-//public class GraphNode implements IVertex<GraphNode, WayEdge> {
-//
-//  /**
-//   * These are fields for this class.
-//   *
-//   * id - a String, which represents the id of this node from the database
-//   *
-//   * latitude - a double representing the latitude of the node
-//   *
-//   * longitude - a double representing the longitude of the node
-//   *
-//   * edgesList - a list of the way edges that start from this node
-//   *
-//   * proxy - a MapsProxy, which is used to load in data from the SQL file
-//   *
-//   * radius - a double representing the radius of the Earth
-//   */
-//  private String id;
-//  private double latitude;
-//  private double longitude;
-//  private List<WayEdge> edgesList;
-//  private MapsProxy proxy;
-//  private final double radius = 6371.0;
-//
-//  /**
-//   * This is the constructor for this class.
-//   *
-//   * @param id       - a String representing the ID of the actor
-//   * @param lat      - a double representing the latitude of the node
-//   * @param lon      - a double representing the longitude of the node
-//   * @param waysList - a List of WayEdges, where the ways are outgoing edges
-//   *                 from this node
-//   * @param proxy    - a MapsProxy, which is used to load in data from the SQL
-//   *                 file
-//   */
-//  public GraphNode(String id, double lat, double lon, List<WayEdge> waysList,
-//      MapsProxy proxy) {
-//    this.id = id;
-//    this.latitude = lat;
-//    this.longitude = lon;
-//    this.edgesList = waysList;
-//    this.proxy = proxy;
-//  }
-//
-//  /**
-//   * This method returns the ID of the node.
-//   *
-//   * @return a String representing the ID of the node
-//   */
-//  public String getNodeID() {
-//    return id;
-//  }
-//
-//  /**
-//   * This method returns the latitude of the node.
-//   *
-//   * @return a double representing the latitude of the node
-//   */
-//  public double getLat() {
-//    return latitude;
-//  }
-//
-//  /**
-//   * This method returns the longitude of the node.
-//   *
-//   * @return a double representing the longitude of the node
-//   */
-//  public double getLon() {
-//    return longitude;
-//  }
-//
-//  @Override
-//  public List<WayEdge> getEdges() {
-//    if (!edgesList.isEmpty()) {
-//      return edgesList;
-//    }
-//
-//    try {
-//      LoadingCache<GraphNode, List<WayEdge>> nodeWayEdgeCache = proxy
-//          .getNodeWayEdgeCache();
-//
-//      if (nodeWayEdgeCache == null) {
-//        System.err.println(
-//            "ERROR: the database must be set before the cache can be used");
-//      }
-//
-//      // get the outgoing edges for this Actor
-//      List<WayEdge> newWayEdges = nodeWayEdgeCache.get(this);
-//
-//      if (newWayEdges == null) {
-//        // an SQLException occurred
-//        System.err.println("ERROR: problem accessing the SQL file");
-//        return null;
-//      }
-//
-//      for (WayEdge wayEdge : newWayEdges) {
-//        edgesList.add(wayEdge);
-//      }
-//
-//      return edgesList;
-//
-//    } catch (ExecutionException execErr) {
-//      System.err.println("ERROR: An error occurred accessing the cache");
-//      return null;
-//    }
-//  }
-//
-//  @Override
-//  public boolean equals(Object obj) {
-//    if (!(obj instanceof GraphNode)) {
-//      return false;
-//    }
-//
-//    // compare using IDs because IDs are unique
-//    GraphNode otherGraphNode = (GraphNode) obj;
-//    return otherGraphNode.getNodeID().equals(this.id);
-//  }
-//
-//  @Override
-//  public int hashCode() {
-//    return id.hashCode();
-//  }
-//
-//  @Override
-//  public double getHeuristic(GraphNode node1, GraphNode node2) {
-//    double lat1 = node1.getLat();
-//    double lon1 = node1.getLon();
-//    double lat2 = node2.getLat();
-//    double lon2 = node2.getLon();
-//
-//    double totalLat = Math.toRadians(lat2 - lat1);
-//    double totalLon = Math.toRadians(lon2 - lon1);
-//    double radLat1 = Math.toRadians(lat1);
-//    double radLat2 = Math.toRadians(lat2);
-//
-//    double transitory = Math.pow(Math.sin(totalLat / 2.0), 2.0)
-//        + (Math.cos(radLat1) * Math.cos(radLat2)
-//            * Math.pow(Math.sin(totalLon / 2.0), 2.0));
-//    double distance = (2.0 * radius * Math.asin(Math.sqrt(transitory)));
-//    return distance;
-//  }
-//
-//}
+package edu.brown.cs.student.api.graph;
+
+import java.util.List;
+
+import edu.brown.cs.student.api.tripadvisor.objects.Item;
+
+/**
+ * A class representing a GraphNode object.
+ */
+public class GraphNode implements IVertex<GraphNode, WayEdge> {
+
+  /**
+   * These are fields for this class.
+   *
+   * id - a String, which represents the id of this node from the database
+   *
+   * latitude - a double representing the latitude of the node
+   *
+   * longitude - a double representing the longitude of the node
+   *
+   * edgesList - a list of the way edges that start from this node
+   *
+   * proxy - a MapsProxy, which is used to load in data from the SQL file
+   *
+   * radius - a double representing the radius of the Earth
+   */
+  private Item item;
+  private double latitude;
+  private double longitude;
+  private List<WayEdge> edgesList;
+  private final double radius = 6371.0;
+
+  /**
+   * This is the constructor for this class.
+   *
+   * @param item     - data of the node of type Item
+   * @param lat      - a double representing the latitude of the node
+   * @param lon      - a double representing the longitude of the node
+   * @param waysList - a List of WayEdges, where the ways are outgoing edges from
+   *                 this node
+   * @param proxy    - a MapsProxy, which is used to load in data from the SQL
+   *                 file
+   */
+  public GraphNode(Item item, double lat, double lon, List<WayEdge> edgesList) {
+    this.item = item;
+    this.latitude = lat;
+    this.longitude = lon;
+    this.edgesList = edgesList;
+  }
+
+  /**
+   * This method returns the latitude of the node.
+   *
+   * @return a double representing the latitude of the node
+   */
+  public double getLat() {
+    return latitude;
+  }
+
+  /**
+   * This method returns the longitude of the node.
+   *
+   * @return a double representing the longitude of the node
+   */
+  public double getLon() {
+    return longitude;
+  }
+
+  public void setEdges(List<WayEdge> edgesList) {
+    this.edgesList = edgesList;
+  }
+
+  @Override
+  public List<WayEdge> getEdges() {
+    return edgesList;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof GraphNode)) {
+      return false;
+    }
+
+    // compare using IDs because IDs are unique
+    GraphNode otherGraphNode = (GraphNode) obj;
+    return otherGraphNode.getItem().equals(this.item);
+  }
+
+  @Override
+  public int hashCode() {
+    return item.hashCode();
+  }
+
+  public Item getItem() {
+    return item;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public List<WayEdge> getEdgesList() {
+    return edgesList;
+  }
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public void setItem(Item item) {
+    this.item = item;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  public void setEdgesList(List<WayEdge> edgesList) {
+    this.edgesList = edgesList;
+  }
+
+  @Override
+  public double getHeuristic(GraphNode node1, GraphNode node2) {
+    double lat1 = node1.getLat();
+    double lon1 = node1.getLon();
+    double lat2 = node2.getLat();
+    double lon2 = node2.getLon();
+
+    double totalLat = Math.toRadians(lat2 - lat1);
+    double totalLon = Math.toRadians(lon2 - lon1);
+    double radLat1 = Math.toRadians(lat1);
+    double radLat2 = Math.toRadians(lat2);
+
+    double transitory = Math.pow(Math.sin(totalLat / 2.0), 2.0)
+        + (Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(totalLon / 2.0), 2.0));
+    double distance = (2.0 * radius * Math.asin(Math.sqrt(transitory)));
+    return distance;
+  }
+
+}
--- src/main/java/edu/brown/cs/student/api/graph/PlanMyDayGraph.java
+++ src/main/java/edu/brown/cs/student/api/graph/PlanMyDayGraph.java
@@ -1,116 +1,56 @@
 //package edu.brown.cs.student.api.graph;
 //
-//import java.util.HashMap;
+//import java.util.ArrayList;
 //import java.util.List;
-//import java.util.Map;
 //
-//import org.json.JSONObject;
-//
-//import com.google.common.collect.ImmutableMap;
-//
+//import edu.brown.cs.student.api.tripadvisor.objects.Attraction;
 //import edu.brown.cs.student.api.tripadvisor.objects.Restaurant;
-//import edu.brown.cs.student.api.tripadvisor.querier.TripAdvisorQuerier;
-//import edu.brown.cs.student.api.tripadvisor.request.RestaurantRequest;
-//import edu.brown.cs.student.chat.gui.Constants;
-//import spark.QueryParamsMap;
 //
 //public class PlanMyDayGraph {
+//  private List<Restaurant> restaurants; // containing 3 Restaurants picked
+//  private List<Attraction> attractions;
+//  private final double EARTH_RADIUS = 6371.0;
 //
-//  public PlanMyDayGraph() {
-//
+//  public PlanMyDayGraph(List<Restaurant> restaurants, List<Attraction> attractions) {
+//    this.restaurants = restaurants;
+//    this.attractions = attractions;
 //  }
 //
-//  public List<Restaurant> getThreeRestaurants {
-//    TripAdvisorQuerier querier = new TripAdvisorQuerier();
-//
-//    // max miles from location; either 1, 2, 5, or 10
-//    String miles = qm.value("miles");
-//    // of the form "[lat] [lon]"
-//    String[] locationStrings = qm.value("location").split(" ");
-//    String lat = locationStrings[0];
-//    String lon = locationStrings[1];
-//    // min rating; options: any, "2 stars", "3 stars", "4 stars", or "5 stars"
-//    String rating = qm.value("rating").replaceAll("[^0-5.,]", "");
-//
-//    if (rating.equals("")) {
-//      rating = "0";
-//    }
-//
-//    /*
-//     * format: a string of cuisine categories of the form "type1,type2,type3";
-//     * (there are no spaces after commas) options: Any, american, barbecue, chinese,
-//     * italian, indian, japanese, mexican, seafood, thai
-//     *
-//     */
-//    String[] cuisinesArr = qm.value("cuisines").toLowerCase().split(",");
-//    String cuisines = "";
-//    if (cuisinesArr.length != 0) {
-//      for (int i = 0; i < cuisinesArr.length; i++) {
-//        /*
-//         * Can only run query using unique code corresponding to each cuisine type.
-//         * Conversion from cuisine type name to code is necessary.
-//         */
-//        if (Constants.CUISINE_TYPE_TO_CODE.containsKey(cuisinesArr[i])) {
-//          cuisines += Constants.CUISINE_TYPE_TO_CODE.get(cuisinesArr[i]) + ",";
-//        }
+//  public List<WayEdge> buildGraph() {
+//    for (int i = 0; i < attractions.size(); i++) {
+//      Attraction attr = attractions.get(i);
+//      List<WayEdge> edgesList = new ArrayList<WayEdge>();
+//      GraphNode node1 = new GraphNode(attr, attr.getLatitude(), attr.getLongitude(), null);
+//      for (int j = 0; j < restaurants.size(); j++) {
+//        Restaurant rest = restaurants.get(j);
+//        GraphNode node2 = new GraphNode(rest, attr.getLatitude(), attr.getLongitude(), null);
+//        WayEdge edge = new WayEdge(this.calculateDistance(node1, node2), node1, node2);
 //      }
-//      cuisines = cuisines.substring(0, cuisines.length() - 1);
+//
 //    }
+//    return null;
+//  }
 //
-//    // options: any, $, $$-$$$, $$$$
-//    String price = Constants.RESTAURANT_PRICE_TO_CODE.get(qm.value("price"));
+//  /**
+//   * This method calculates the Haversine distance between two nodes.
+//   *
+//   * @param node1 GraphNode
+//   * @param node2 GraphNode
+//   * @return Haversine distance between two nodes
+//   */
+//  public double calculateDistance(GraphNode node1, GraphNode node2) {
+//    double lat1 = node1.getLat();
+//    double lon1 = node1.getLon();
+//    double lat2 = node2.getLat();
+//    double lon2 = node2.getLon();
+//    double radLat1 = Math.toRadians(lat1);
+//    double radLat2 = Math.toRadians(lat2);
+//    double totalLat = Math.toRadians(lat2 - lat1);
+//    double totalLon = Math.toRadians(lon2 - lon1);
 //
-//    /*
-//     * dietary restrictions; options: "None", "Vegetarian friendly",
-//     * "Vegan options", "Halal", "Gluten-free options
-//     */
-//    String[] dietArr = qm.value("diet").toLowerCase().split(",");
-//    String dietStr = "";
-//    if (dietArr.length != 0) {
-//      for (int i = 0; i < dietArr.length; i++) {
-//        if (Constants.DIETARY_RESTRICTION_TO_CODE.containsKey(dietArr[i])) {
-//          dietStr += Constants.DIETARY_RESTRICTION_TO_CODE.get(dietArr[i]) + ",";
-//        }
-//      }
-//      dietStr = dietStr.substring(0, dietStr.length() - 1);
-//    }
-//
-//    Map<String, Object> params = new HashMap<>();
-//    params.put("limit", Constants.LIMIT);
-//    params.put("lang", Constants.LANG);
-//    params.put("currency", Constants.CURRENCY);
-//    params.put("lunit", Constants.LUNIT);
-//    params.put("latitude", lat);
-//    params.put("longitude", lon);
-//    params.put("min_rating", rating);
-//    params.put("dietary_restrictions", dietStr);
-//    params.put("distance", miles);
-//    params.put("combined_food", cuisines);
-//    params.put("prices_restaurants", price);
-//
-//    String errorMsg = paramsAreValid(params);
-//    // Parameters are invalid.
-//    if (!errorMsg.equals("")) {
-//      System.out.println(errorMsg);
-//      Map<String, String> variables = ImmutableMap.of("restaurants_result", "",
-//          "restaurants_errors", errorMsg);
-//      return new JSONObject(variables);
-//    }
-//
-//    List<Restaurant> restaurants = querier.getRestaurants(new RestaurantRequest(params));
-//
-//    StringBuilder sb = new StringBuilder();
-//    if (restaurants.isEmpty()) {
-//      sb.append("No matching result.");
-//    } else {
-//      for (int i = 0; i < restaurants.size() - 1; i++) {
-//        sb.append(restaurants.get(i).toStringHTML() + "<hr>");
-//      }
-//      sb.append(restaurants.get(restaurants.size() - 1).toStringHTML());
-//    }
-//
-//    Map<String, String> variables = ImmutableMap.of("restaurants_result", sb.toString(),
-//        "restaurants_errors", "");
-//    return new JSONObject(variables);
+//    double transitory = Math.pow(Math.sin(totalLat / 2.0), 2.0)
+//        + (Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(totalLon / 2.0), 2.0));
+//    double distance = (2.0 * EARTH_RADIUS * Math.asin(Math.sqrt(transitory)));
+//    return distance;
 //  }
 //}
